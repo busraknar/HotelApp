89a52733ea494241ab225b6934478c605e3e9aae
busraknar <b.ertas91@gmail.com>
2024-08-11 15:25:00 +0300

    RoomTypeController edited

--- HotelApp/Controllers/RoomTypeController.cs
+++ HotelApp/Controllers/RoomTypeController.cs
@@ -1,12 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DataAccessLayer.Data;
+using Microsoft.AspNetCore.Mvc;
 
 namespace HotelApp.Controllers
 {
     public class RoomTypeController : Controller
     {
+        private readonly HotelDbContext _context;
+
+        public RoomTypeController(HotelDbContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var RoomTypeList = _context.RoomTypes.ToList();
+            return View(RoomTypeList);
         }
     }
 }
