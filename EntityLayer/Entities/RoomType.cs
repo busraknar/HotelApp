64f0f3ccc53f037940e78183ffdf043083ab723e
busraknar <b.ertas91@gmail.com>
2024-08-19 00:58:13 +0300

    RoomType Entities edited

--- EntityLayer/Entities/RoomType.cs
+++ EntityLayer/Entities/RoomType.cs
@@ -6,7 +6,13 @@ namespace EntityLayer.Entities
     {
         [Key]
         public int RoomTypeId { get; set; }
+
+        [Display(Name = "Oda Adı")]
+        [Required(ErrorMessage = "Oda adını boş bırakamazsınız!")]
         public string RoomName { get; set; } = string.Empty;
+
+        [Display(Name = "Oda Fiyatı")]
+        [Required(ErrorMessage = "Oda fiyatını boş bırakamazsınız!")]
         public double RoomPrice { get; set; }
         public virtual List<Room>? Rooms { get; set; }
 
